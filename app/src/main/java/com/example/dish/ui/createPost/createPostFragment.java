377f58bc687c005e03009156c896b29e2315a71a
aperng2 <aperng223@gmail.com>
2021-04-21 16:17:21 -0500

    add image upload functionality

--- app/src/main/java/com/example/dish/ui/createPost/createPostFragment.java
+++ app/src/main/java/com/example/dish/ui/createPost/createPostFragment.java
@@ -1,10 +1,16 @@
 package com.example.dish.ui.createPost;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.RadioButton;
@@ -16,9 +22,14 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.dish.R;
 
+import static android.app.Activity.RESULT_OK;
+
 
 public class createPostFragment extends Fragment {
     //private createPostViewModel createPostVM;
+
+    int RESULT_LOAD_IMAGE = 1;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         //createPostVM = new ViewModelProvider(this).get(createPostViewModel.class);
@@ -32,6 +43,7 @@ public class createPostFragment extends Fragment {
             }
         });*/
 
+        /* post type (donation/event) radio button selection */
         final RadioGroup postTypeRadioGroup = root.findViewById(R.id.rgDonationEvent);
         final RadioButton donationButton = root.findViewById(R.id.rbDonation);
         final RadioButton eventButton = root.findViewById(R.id.rbEvent);
@@ -61,6 +73,23 @@ public class createPostFragment extends Fragment {
         });
 
 
+        /* hide default image initially */
+        ImageView imgView = root.findViewById(R.id.create_post_img_view);
+        imgView.setVisibility(View.GONE);
+        /* add post image */
+        Button buttonLoadImage = (Button) root.findViewById(R.id.create_post_img_button);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+
+        /* POST button */
         final Button btCreatePost = root.findViewById(R.id.btCreatePost);
         btCreatePost.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,5 +98,18 @@ public class createPostFragment extends Fragment {
             }
         });
         return root;
+    }
+
+
+    /* show uploaded image after media image select */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri imageUri = data.getData();
+            ImageView imageView = (ImageView) getView().findViewById(R.id.create_post_img_view);
+            imageView.setImageURI(imageUri);
+            imageView.setVisibility(View.VISIBLE);
+        }
     }
 }
