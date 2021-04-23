1713c843013e0ca7d8db50894954e5fcc936a9f7
sandhya <ssivak3@illinois.edu>
2021-04-22 23:29:44 -0500

    added database activity ......... who knows if it works ..... not me

---
+++ app/src/main/java/com/example/dish/dbactivity.java
@@ -0,0 +1,63 @@
+package com.example.dish;
+
+import java.sql.*;
+
+import android.os.AsyncTask;
+
+public class dbactivity extends android.app.Activity {
+
+    private class psqlcon extends AsyncTask<Void, Void, Void> {
+        public psqlcon() {
+            super();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Connection conn = null;
+            Statement st = null;
+
+            try {
+                Class.forName("org.postgresql.Driver");
+                System.out.println("connecting to db !! ");
+                conn = DriverManager.getConnection("jdbc:postgresql://domain.com:5432/DISH", "root", "root");
+
+                System.out.println("execute query ... ? ");
+                st = conn.createStatement();
+                String query;
+                query = "SELECT id FROM users;";
+                ResultSet rs = st.executeQuery(query);
+
+                while (rs.next()) {
+                    System.out.println(rs.getString("id"));
+                }
+
+                rs.close();
+                st.close();
+                conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (st != null) {
+                    st.close();
+                }
+            } catch (SQLException see) {
+                System.out.println("oops");
+            }
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
