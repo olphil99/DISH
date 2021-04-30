ba3eea6532455cb625ea543a54cdf5cb74bdc7cc
aperng2 <aperng223@gmail.com>
2021-04-30 10:10:01 -0500

    luke: fabulized send text - put margin after text box, color, font, etc (65defe4 without postgre changes)

--- app/src/main/java/com/example/dish/ui/forum/forumActivity.java
+++ app/src/main/java/com/example/dish/ui/forum/forumActivity.java
@@ -2,8 +2,12 @@ package com.example.dish.ui.forum;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Button;
 import android.content.DialogInterface;
@@ -23,7 +27,7 @@ public class forumActivity extends AppCompatActivity {
         String name = intent.getExtras().getString("name");
         TextView fName = (TextView) findViewById(R.id.forumName);
         fName.setText(name);
-        Button subscribe = (Button) findViewById(R.id.subscribe);
+        TextView subscribe = (TextView) findViewById(R.id.subscribe);
         subscribe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String current = subscribe.getText().toString();
@@ -32,22 +36,50 @@ public class forumActivity extends AppCompatActivity {
                     subscribe.setText("unsubscribe");
                 }else{
                     //unsubscribe action
+
                     subscribe.setText("subscribe");
                 }
             }
         });
 
-        Button send = (Button) findViewById(R.id.sendb);
+        TextView send = (TextView) findViewById(R.id.sendb);
         send.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText editText = (EditText) findViewById(R.id.message);
                 EditText name = (EditText) findViewById(R.id.id);
                 String idname = name.getText().toString();
                 String message = editText.getText().toString();
-                TextView display = (TextView) findViewById(R.id.display);
-                String prev = display.getText().toString();
+                LinearLayout display = (LinearLayout) findViewById(R.id.display);
+
+                LinearLayout temp = new LinearLayout(display.getContext());
+                temp.setOrientation(LinearLayout.VERTICAL);
+                TextView nameView = new TextView(temp.getContext());
+                TextView tv = new TextView(temp.getContext());
+                // layout params
+                //float factor = this.getContext().getResources().getDisplayMetrics().density;
+
+                // other params
+                nameView.setText(idname + ":");
+                display.setMinimumWidth(nameView.getWidth());
+
+                float factor = display.getContext().getResources().getDisplayMetrics().density;
+                tv.setText(message);
+
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT );
+                layoutParams.leftMargin = (int)(15* factor);
+                layoutParams.rightMargin =(int)(15* factor);
+                tv.setLayoutParams(layoutParams);
+                tv.setPadding(5,2,5,2);
+                tv.setGravity(Gravity.CENTER);
+                tv.setBackgroundResource(R.drawable.back);
+                tv.setTextColor(Color.BLACK);
+                tv.setFocusable(true);
+
+                //String prev = display.getText().toString();
                 //dh.insertValues(message);
-                display.setText(prev + "\n \n" + idname + ":" + "\n\n" + message);
+                temp.addView(nameView);
+                temp.addView(tv);
+                display.addView(temp);
             }
         });
 
